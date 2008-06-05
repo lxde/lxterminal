1b2cb5aaf64136dd72be0aceaab912ad779ed6b5
Fred Chien <fred@lxde.org>
2008-06-05 16:03:46 +0000

    Add Missing files.

---
+++ src/setting.c
@@ -0,0 +1,50 @@
+/*
+ *      Copyright 2008 Fred Chien <cfsghost@gmail.com>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+#include <glib.h>
+#include <gtk/gtk.h>
+#include <glib/gi18n.h>
+
+#include "setting.h"
+
+Setting *load_setting_from_file(const char *filename)
+{
+	GKeyFile *keyfile;
+	GKeyFileFlags flags;
+	GError *error = NULL;
+	Setting *setting;
+
+	/* initiate key_file */
+	keyfile = g_key_file_new();
+	flags = G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS;
+
+	/* Load config */
+	if (!g_key_file_load_from_file (keyfile, filename, flags, &error)) {
+		g_error ("[conf-file] %s", error->message);
+		return NULL;
+	}
+
+
+	setting = (Setting *)malloc(sizeof(Setting));
+
+	/* general setting */
+	setting->fontname = g_key_file_get_string(keyfile, "general", "fontname", NULL);
+
+	return setting;
+}
---
+++ src/setting.h
@@ -0,0 +1,10 @@
+#ifndef LXTERMINAL_SETTING_H
+#define LXTERMINAL_SETTING_H
+
+typedef struct _setting {
+	char *fontname;
+} Setting;
+
+Setting *load_setting_from_file(const char *filename);
+
+#endif
