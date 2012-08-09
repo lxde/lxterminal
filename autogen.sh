f71c5ff62071e574054d13344c9215c508bc5ce1
Vadim Ushakov <igeekless@gmail.com>
2012-08-09 16:48:46 +0800

    allow automake 1.12

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,8 @@ AUTOMAKE=${AUTOMAKE:-automake}
 AM_INSTALLED_VERSION=$($AUTOMAKE --version | sed -e '2,$ d' -e 's/.* \([0-9]*\.[0-9]*\).*/\1/')
 
 if [ "$AM_INSTALLED_VERSION" != "1.10" \
-    -a "$AM_INSTALLED_VERSION" != "1.11" ];then
+    -a "$AM_INSTALLED_VERSION" != "1.11" \
+    -a "$AM_INSTALLED_VERSION" != "1.12" ];then
 	echo
 	echo "You must have automake > 1.10 or 1.11 installed to compile lxterminal."
 	echo "Install the appropriate package for your distribution,"
