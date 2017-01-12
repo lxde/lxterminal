e2ad448556ee0f78ebdd0e36dc16e96702326fb6
Yao Wei (魏銘廷) <mwei@lxde.org>
2017-01-13 00:02:45 +0800

    fix: display dialog buttons for changing tab name
    Closes #30 on GitHub.

--- src/lxterminal.c
+++ src/lxterminal.c
@@ -573,8 +573,8 @@ static void terminal_name_tab_activate_event(GtkAction * action, LXTerminal * te
         _("Name Tab"),
         GTK_WINDOW(terminal->window),
         0,
-        NULL, GTK_RESPONSE_CANCEL,
-        NULL, GTK_RESPONSE_OK,
+        _("_Cancel"), GTK_RESPONSE_CANCEL,
+        _("_OK"), GTK_RESPONSE_OK,
         NULL);
     gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_OK);
     if (gtk_icon_theme_has_icon(gtk_icon_theme_get_default(), "lxterminal"))
