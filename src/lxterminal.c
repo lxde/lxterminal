lxde/lxterminal

commit a7a7162206e1f0375a92f39facdea883b68dec4c

fix: Fix VTE background echoes
This workaround is found in vte/src/app/app.cc, that avoiding letting
window painting the background can avoid background echoes.  However this
will cause other elements in the terminal window to have their background
transparent if not set.  To workaround this, we have to add background
color to the vbox of the window.

src/lxterminal.c
@@ -1787,6 +1787,27 @@ static void terminal_settings_apply(LXTerminal * terminal)
     /* Reinitialize "composited". */
     terminal->rgba = gdk_screen_is_composited(gtk_widget_get_screen(terminal->window));
 
+    #if GTK_CHECK_VERSION (2, 90, 8)
+    /* Found in vteapp as a workaround.  Related bug:
+     * https://bugzilla.gnome.org/show_bug.cgi?format=multiple&id=729884 */
+    gboolean has_transparency = setting->background_color.alpha < 1.0;
+    gtk_widget_set_app_paintable(
+        GTK_WIDGET(terminal->window), has_transparency);
+
+    /* De-transarent box */
+    GtkCssProvider* box_css_provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_data(box_css_provider,
+        "box{background-color:@theme_bg_color;}",
+        -1, NULL
+    );
+
+    GtkStyleContext* box_style_ctx =
+        gtk_widget_get_style_context(GTK_WIDGET(terminal->box));
+    gtk_style_context_add_provider(
+        box_style_ctx, box_css_provider,
+        GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    #endif
+
     /* Update tab position. */
     terminal->tab_position = terminal_tab_get_position_id(setting->tab_position);
     terminal_tab_set_position(terminal->notebook, terminal->tab_position);

commit 179948014a7df0e9b44fd682b0267318e6cb7b0c

Re-enable compiling with vte-2.90
GdkColor has no member named 'alpha'.

This fixes a regression introduced in
commit a7a7162206e1f0375a92f39facdea883b68dec4c,
github pull request #100.

src/lxterminal.c
@@ -1805,7 +1805,7 @@ static void terminal_settings_apply(LXTerminal * terminal)
     /* Reinitialize "composited". */
     terminal->rgba = gdk_screen_is_composited(gtk_widget_get_screen(terminal->window));
 
-    #if GTK_CHECK_VERSION (2, 90, 8)
+    #if GTK_CHECK_VERSION (2, 90, 8) && VTE_CHECK_VERSION (0, 38, 0)
     /* Found in vteapp as a workaround.  Related bug:
      * https://bugzilla.gnome.org/show_bug.cgi?format=multiple&id=729884 */
     gboolean has_transparency = setting->background_color.alpha < 1.0;